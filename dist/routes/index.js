2583efeb57c41ccff1c61139b6d20f323c72f331
Luiz Scheidegger <lscscheidegger@gmail.com>
2022-12-12 15:43:34 +0100

    removed dist folder from git

--- dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const routes = (0, express_1.Router)();
-routes.get('/', function (req, res, next) {
-    res.render("index", { title: "Home", cssFiles: ["style.css"], jsFiles: [] });
-});
-module.exports = routes;
